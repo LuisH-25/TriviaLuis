a7de3072dd52af62aaff77451c71317d722d067f
LUIS ENRIQUE ENRIQUE <113267759+LuisH-25@users.noreply.github.com>
2022-09-19 03:18:09 +0000

    Agregar colores, tiempo y preguntas mostradas por "for"

--- main.py
+++ main.py
@@ -12,114 +12,115 @@ MAGENTA = '\033[35m'
 CYAN = '\033[36m'
 WHITE = '\033[37m'
 RESET = '\033[39m'
+time.sleep(1)  # Espera de 1 segundo
 
 # Variables
-puntaje = random.randint(0, 10)
-
+puntaje = 0
+preguntas = ["","¿Donde se inicio el covid?","¿Cuantas dosis deberias tener si tienes más de 18 años?","¿Que es el SARS-CoV-2 ?","¿En que año se confirmo el paciente cero en el Perú?"]
+alternativas = [["","","",""],["a) China", "b) EE.UU", "c) Rusia","d) Perú"],["a) Ninguna", "b) 2", "c) 3","d) 4"],["a) Proteina", "b) Bacteria", "c) Virus","d) Ninguna de las anteriores"],["a) 2021", "b) 2019", "c) 2020","d) 2022"]]
+respuestas = ["","","","",""]
+respuestas_correctas = ["","a","d","c","c"]
+conforme = "no"
+edad = 0
+nombre = ""
 contador = 0;
-for contador in range(0,3):
+
+time.sleep(1)
+# Mensaje de bievenida
+print (CYAN + "\nBienvenido a mi trivia sobre la pandemia" + RESET)
+print ("Pondremos a prueba tus conocimientos")
+time.sleep(1)
+
+
+while conforme=="no":        # validacion del nombre
+  nombre = input(YELLOW + "\nIngrese nombre:" + RESET)
+  nombre = nombre.lower()
+  time.sleep(1)
+  conforme = input(GREEN + "\n¿" + nombre + " es su nombre? Responda con 'si' o  'no' : " + RESET).lower()
+  while conforme not in ("si", "no"):
+    conforme = input(RED + "\nResponda con 'si' o  'no' por favor: " + RESET).lower()
+    time.sleep(1)
+      
+conforme = "no"
+
+while conforme=="no":        # validacion de la edad
+  edad = input(YELLOW + "\nIngrese su edad:" + RESET)
+  
+  time.sleep(1)
+  while edad.isnumeric() == False:
+    edad = input(RED+"\nIngrese una edad numérica valida "+RESET)
+  conforme = input(GREEN+"\n¿" + edad+" es su edad? Responda con 'si' o  'no' : "+RESET).lower()
+  time.sleep(1)
+  while conforme not in ("si", "no"):
+    conforme = input(RED + "\nResponda con 'si' o  'no' por favor: " + RESET).lower()
+
+time.sleep(1)
+    
+# Instrucciones de la trivia:
+print(YELLOW + "\n Hola", nombre, "responde las siguientes preguntas escribiendo la letra de la alternativa (en mayuscula o minuscula) y presionando 'Enter' para enviar tu respuesta:\n" + RESET)
+puntaje_inicial = random.randint(0, int(edad))
+
+print(GREEN + "Nota:", nombre, ", obtendras puntos a memida que respondas correctamente "+ RESET)
+conforme = input(YELLOW + "Obtuviste un puntaje inicial que esta entre 0 y "+edad+", quieres adivinar tu puntaje (si aciertas tendras puntos extras) Responda con 'si' o  'no'" + RESET)
+while conforme not in ("si", "no"):
+  conforme = input(RED + "\nResponda con 'si' o  'no' por favor: " + RESET).lower()
+  time.sleep(1)
+if(conforme == 'si'):
+  puntaje = input(RED + "\n¿Cual es su puntaje inicial?" + RESET)
+  time.sleep(1)
+  while puntaje.isnumeric() == False:
+    puntaje = input(RED+"\nIngrese un número "+RESET)
+    time.sleep(1)
+  if(puntaje== puntaje_inicial):
+    print(GREEN + "ACERTASTE"+ RESET)
+    puntaje= puntaje *2
+  else:
+    print ("El número a adivinar era: " + str(puntaje_inicial))
+    
+  print ("Tu puntaje inicial es: " + puntaje)
+time.sleep(1)
+print ("Comenzamos en: ")
+for contador in range (3,0,-1):
+  print(contador)
+  time.sleep(1)  # Espera 1 seg
+
+cantidad_preguntas = len(preguntas)
+for x in range (1,cantidad_preguntas):
+  time.sleep(1)
+  print(CYAN + "PREGUNTA " + str(x) + ":\n" + RESET)
+  print(YELLOW+str(x) + ") " + preguntas[x] + RESET)
+  for y in range (0,cantidad_preguntas-1):
+    print(alternativas[x][y])
+  respuestas[x] = input("\nTu respuesta es: ").lower()
+  while respuestas[x] not in ("a", "b", "c", "d"):
+    respuestas[x] = input(RED + "Debes responder a, b, c o d. Ingresa nuevamente tu respuesta: " + RESET)
+    puntaje = puntaje - 1
+  
+  if (respuestas_correctas[x] == respuestas[x]):
+    print (GREEN + "Muy bien " + nombre + " !" + RESET)
+    puntaje = puntaje + 1
+  else:
+    print (RED + "Incorrecto! " + RESET)
+    print (GREEN + "La respuesta correcta es : "+ respuestas_correctas[x] + RESET)
+    puntaje = puntaje - 1
+  ##print ("Tu puntaje es: ", puntaje)
+
+  print("\n")
+  time.sleep(1)  # Espera 1 seg
+  
+condicion= input("\n¿Deseas ver tus respuestas?").lower()
+while condicion not in ("si", "no"):
+  condicion = input("Debes responder si ó no. Ingresa nuevamente tu respuesta: ").lower()
+
+if condicion == "si":
+  for z in range (1,len(respuestas)):
+    print("PREGUNTA " + str(z) + ": " + respuestas[z])
+
+for contador in range(0,5,1):
   print(".")
   time.sleep(1) # Espera 2 segundos antes de continuar.
   
-  
-# Mensaje de bievenida
-print (CYAN + "Bienvenido a mi trivia sobre la pandemia" + RESET)
-print ("Pondremos a prueba tus conocimientos")
-
-nombre = input("Ingresa tu nombre: ")
-
-# Instrucciones de la trivia:
-print("\n Hola", nombre, "responde las siguientes preguntas escribiendo la letra de la alternativa (en mayuscula o minuscula) y presionando 'Enter' para enviar tu respuesta:\n")
-print ("Nota:", nombre, ", obtendras puntos a memida que respondas correctamente\n")
-
-print ("Pregunta 1:\n")
-
-# Pregunta 1
-print ("1) ¿Donde se inicio el covid?")
-print ("a) China")
-print ("b) EE.UU")
-print ("c) Rusia")
-print ("d) Perú")
-respuesta_1 = input("\nTu respuesta: ").lower()
-
-while respuesta_1 not in ("a", "b", "c", "d", "x"):
-  respuesta_1 = input("Debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ")
-
-if (respuesta_1 == "a"):
-  print ("Muy bien", nombre, "!")
-  puntaje = puntaje + 1
-elif (respuesta_1 == "x"):
-  print ("Wuau, encontraste un código oculto", nombre, "!, se te dará puntos adicional")
-  puntaje = puntaje * 5
-else:
-  print ("Incorrecto! ")
-  print ("La respuesta correcta es : a) China")
-print ("Tu puntaje es: ", puntaje)
-
-# Pregunta 2
-print ("\nPregunta 2:\n")
-print ("2) ¿Cuantas dosis deberias tener si tienes más de 18 años?")
-print ("a) Ninguna")
-print ("b) 2")
-print ("c) 3")
-print ("d) 4")
-respuesta_2 = input("\nTu respuesta: ").lower()
-
-while respuesta_2 not in ("a", "b", "c", "d"):
-  respuesta_2 = input("Debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ")
-
-if (respuesta_2 == "d"):
-  print ("Muy bien", nombre, "!")
-  puntaje = puntaje + 1
-else:
-  print ("Incorrecto! ")
-  print ("La respuesta correcta es : d) 4")
-  print ("Ya puedes tener tu 4ta dosis, vacunate")
-print ("Tu puntaje es: ", puntaje)
-
-# Pregunta 3
-print ("\nPregunta 3:\n")
-print ("3) ¿Que es el SARS-CoV-2 ?")
-print ("a) Proteina")
-print ("b) Bacteria")
-print ("c) Virus")
-print ("d) Ninguna de las anteriores")
-respuesta_3 = input("\nTu respuesta: ").lower()
-
-while respuesta_3 not in ("a", "b", "c", "d"):
-  respuesta_3 = input("Debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ")
-
-if (respuesta_3 == "c"):
-  print ("Muy bien", nombre, "!")
-  puntaje = puntaje + 1
-else:
-  print ("Incorrecto! ")
-  print ("La respuesta correcta es : c) Virus")
-  print ("Ya puedes tener tu 4ta dosis, vacunate")
-print ("Tu puntaje es: ", puntaje)
-
-# Pregunta 4
-print ("\nPregunta 4:\n")
-print ("4) ¿En que año se confirmo el paciente cero en el Perú?")
-print ("a) 2021")
-print ("b) 2019")
-print ("c) 2020")
-print ("d) 2022")
-respuesta_4 = input("\nTu respuesta: ").lower()
-
-while respuesta_4 not in ("a", "b", "c", "d"):
-  respuesta_4 = input("Debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ")
-
-if (respuesta_4 == "c"):
-  print ("Muy bien", nombre, "!")
-  puntaje = puntaje + 1
-else:
-  print ("Incorrecto! ")
-  print ("La respuesta correcta es : c) 2020")
-print ("Tu puntaje es: ", puntaje)
-
 print ("Tu puntaje total es: ", puntaje)
-
 print (nombre, ", muchas gracias por participar")
 
 
